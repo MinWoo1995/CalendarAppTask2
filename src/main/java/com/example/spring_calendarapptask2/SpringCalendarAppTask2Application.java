aae36d7fb40dbb6e5d7863d825b3655996d1e3dc
MinWoo1995 <wjsalsdn120@naver.com>
2026-01-08 15:54:03 +0900

    first commit

---
+++ src/main/java/com/example/spring_calendarapptask2/SpringCalendarAppTask2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_calendarapptask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCalendarAppTask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCalendarAppTask2Application.class, args);
+    }
+
+}
